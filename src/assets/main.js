716ce779120d378b6aae0843330a74afd4a8c93e
Yuxun He <Helen@yuxuns-mbp.bentley.com>
2017-07-11 13:30:41 -0400

    sss

--- src/assets/main.js
+++ src/assets/main.js
@@ -52,8 +52,8 @@ function guess() {
     }
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (!validateInput(input)) {
-      return false;
+    if (!validateInput(input.value)) {
+      return;
     } else {
       attempt.value++;
     }
